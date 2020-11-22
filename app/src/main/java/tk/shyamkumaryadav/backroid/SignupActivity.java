39f83ff01810b4724dd998d0d046d9e6ca4e63e8
shyamkumaryadav <shyamkumaryadav2003@gmail.com>
2020-11-22 19:01:19 +0530

    icon change

--- app/src/main/java/tk/shyamkumaryadav/backroid/SignupActivity.java
+++ app/src/main/java/tk/shyamkumaryadav/backroid/SignupActivity.java
@@ -5,13 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private TextInputLayout emailLayout, password1Layout, password2Layout;
+    private TextInputEditText email, password1, password2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        email = findViewById(R.id.emailEditText);
+        password1 = findViewById(R.id.password1EditText);
+        password2 = findViewById(R.id.password2EditText);
+        emailLayout = findViewById(R.id.emailLayout);
+        password1Layout = findViewById(R.id.password1Layout);
+        password2Layout = findViewById(R.id.password2Layout);
     }
 
     public void loginView(View view) {
